6e5ebd5d4579f469dc09627eee7d5974002cf31a
anil2003shukla <shuklaanil0807@gmail.com>
2024-07-14 19:52:59 +0530

    Project Commit

--- Loan Calculator/Index.js
+++ Loan Calculator/Index.js
@@ -5,16 +5,10 @@ const paymentEle=document.querySelector('#Payment');
 const calcuEle=document.querySelector('.calcu');
 function calculation(){
     const loanAmount=Number(loanEle.value);
-    console.log(loanAmount);
     const interestRate=Number(interestEle.value);
-    console.log(interestRate);
     const month=Number(monthEle.value);
-    console.log(month);
-    const time=month/12;
-    console.log(time);
-    const interest=(loanAmount*interestRate*time)/100;
-    const payment=(loanAmount+interest)/month;
-    console.log(payment);
+    const interest=(loanAmount*interestRate*0.01)/month;
+    const payment=(loanAmount/month+interest);
     return payment;
 }
 calcuEle.addEventListener('click',()=>{
